d56af8a10cfa37119e0b4495f1c1bb6ee0f4c9e4
lounis <lounis@fusacq.com>
2017-02-16 10:05:25 +0100

    les proprietes et les evenement

--- app/app.component.js
+++ app/app.component.js
@@ -10,13 +10,28 @@ var RoomyAppComponent = (function () {
     function RoomyAppComponent() {
         this.title = "Ronny chez Fusacq";
     }
+    Object.defineProperty(RoomyAppComponent.prototype, "isConnected", {
+        get: function () {
+            return this.user == null;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    RoomyAppComponent.prototype.onButtonConnectClick = function (event) {
+        console.log(event);
+        this.user = { name: 'Lounis' };
+        event.stopPropagation();
+    };
+    RoomyAppComponent.prototype.onSpaceKeyup = function () {
+        console.log(this.user.name);
+    };
     return RoomyAppComponent;
 }());
 RoomyAppComponent = __decorate([
     core_1.Component({
         selector: 'roomy-app',
         //Le ? pour dire user est optionnelle (operateur de navigateur securisé)
-        template: "<h1> Formation Angular - {{ title }} </h1>\n                <h2>Bienvenu {{ user?.name }}</h2> \n                .........\n                <footer>\n                    <roomy-topfive> </roomy-topfive>\n                </footer>\n                "
+        template: "<h1>Formation Angular - {{title}}</h1>\n                <nav class=\"navbar navbar-default navbar-static-top\">\n                    <div class=\"container-fluid\">\n                        <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">\n                            <ul class=\"nav navbar-nav\">\n                                <li><a href=\"#\"></a></li>\n                                <li><a href=\"#\"></a></li>\n                                <li><a href=\"#\"></a></li>\n                                <li><a href=\"#\"></a></li>\n                            </ul>\n                        </div>\n                   \n                        <form class=\"navbar-form navbar-right\">\n                            <div class=\"form-group\">\n                                <input type=\"text\" class=\"form-control\" placeholder=\"Login\" (keyup.space)=\"onSpaceKeyup()\">\n                            </div>\n                            <div class=\"form-group\">\n                                <input type=\"password\" class=\"form-control\">\n                            </div>\n                            <button type=\"button\" class=\"btn btn-default\"\n                            (click)=\"onButtonConnectClick($event)\">Se connecter</button>\n                        </form>\n                     </div>\n                </nav>\n                <p [hidden] = \"isConnected\">Bienvenu {{user?.name}}</p>\n                ....\n                <roomy-topfive>\n                </roomy-topfive>\n\n                "
     })
 ], RoomyAppComponent);
 exports.RoomyAppComponent = RoomyAppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,53 @@ import { Component } from '@angular/core';
 @Component({
     selector : 'roomy-app',
     //Le ? pour dire user est optionnelle (operateur de navigateur securisé)
-    template : `<h1> Formation Angular - {{ title }} </h1>
-                <h2>Bienvenu {{ user?.name }}</h2> 
-                .........
-                <footer>
-                    <roomy-topfive> </roomy-topfive>
-                </footer>
+    template : `<h1>Formation Angular - {{title}}</h1>
+                <nav class="navbar navbar-default navbar-static-top">
+                    <div class="container-fluid">
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#"></a></li>
+                                <li><a href="#"></a></li>
+                                <li><a href="#"></a></li>
+                                <li><a href="#"></a></li>
+                            </ul>
+                        </div>
+                   
+                        <form class="navbar-form navbar-right">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Login" (keyup.space)="onSpaceKeyup()">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control">
+                            </div>
+                            <button type="button" class="btn btn-default"
+                            (click)="onButtonConnectClick($event)">Se connecter</button>
+                        </form>
+                     </div>
+                </nav>
+                <p [hidden] = "isConnected">Bienvenu {{user?.name}}</p>
+                ....
+                <roomy-topfive>
+                </roomy-topfive>
+
                 `
 })
 export class RoomyAppComponent{
         title:string = "Ronny chez Fusacq";
         user:any; // = {name:'Lounis'};
+
+        get isConnected(){
+            return this.user == null;
+        }
+
+        onButtonConnectClick(event){
+            console.log(event);
+            this.user = {name : 'Lounis'};
+            event.stopPropagation();
+
+        }
+
+        onSpaceKeyup(){
+            console.log(this.user.name);
+        }
 }
